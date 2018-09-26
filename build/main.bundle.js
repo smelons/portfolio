397ba3fe930b20b46d89dbe12456d0d6d198a541
Mel Bailey <mbailey@ethosgroup.com>
2018-09-26 01:37:03 -0500

    preact

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,1 +1,20 @@
 "use strict";
+
+var _preact = require("preact");
+
+(0, _preact.render)((0, _preact.h)(
+    "div",
+    { id: "foo" },
+    (0, _preact.h)(
+        "span",
+        null,
+        "Hello, world!"
+    ),
+    (0, _preact.h)(
+        "button",
+        { onClick: function onClick(e) {
+                return alert("hi!");
+            } },
+        "Click Me"
+    )
+), document.body);
