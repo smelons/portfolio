20a98e95ec96d8e58a04107900ebbb2af3fd73bd
Mel Bailey <mbailey@ethosgroup.com>
2018-11-21 21:49:36 -0600

    added sass processing, created main section

---
+++ src/components/about.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export class About extends Component {
+  render() {
+    return (
+      <section class="hero is-dark is-fullheight">
+        <div class="hero-head">
+        </div>
+        <div class="hero-body">
+          <div class="container has-text-centered">
+            <h1 class="title">
+              Mel Bailey
+            </h1>
+            <h2 class="subtitle">
+              Full Stack Developer, DevOps Engineer
+            </h2>
+          </div>
+        </div>
+        <div class="hero-foot">
+        </div>
+      </section>
+    );
+  }
+}
