b5d919837ae34c9a47c1d405e96227a7342cf9cc
Mel Bailey <mbailey@ethosgroup.com>
2018-11-23 03:52:56 -0600

    basic navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { About } from './components/about'
+import Navigation from './components/Navigation/Navigation'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <About></About>
+      <Navigation></Navigation>
     );
   }
 }
